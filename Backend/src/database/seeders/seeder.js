c400c078210da312e13ddc7bdb956dce70a6a545
NacherBeats <ibentivoglioacademico@gmail.com>
2026-06-29 17:01:35 -0300

    Feat: Se separa el backend de proyecto 2die4, se crea seeder para poblar la base de datos

--- Backend/src/database/seeders/seeder.js
+++ Backend/src/database/seeders/seeder.js
@@ -15,130 +15,122 @@ const conectarEjecutar = async () => {
 
         const productosPrueba = [
             {
-                nombre: "Remera Caminito Oversize",
-                descripcion: "Corte urbano pesado con estampa inspirada en los pasajes del arrabal.",
+                nombre: "Remera Oversize",
+                descripcion: "Corte urbano pesado con estampa DISCIPLINE.",
                 precio: 24500,
-                imagen: "",
+                imagen: "https://i.ibb.co/S4m8Wbsv/Remera-DISCIPLINE.png",
                 edicionLimitada: true,
                 tallesStock: { "S": 5, "M": 12, "L": 8, "XL": 4 }
             },
             {
-                nombre: "Remera Lunfardo Black",
-                descripcion: "Algodón prémium con tipografía gótica y jerga callejera de Buenos Aires.",
+                nombre: "Remera Banda",
+                descripcion: "Algodón prémium Soda Estereo.",
                 precio: 22000,
-                imagen: "",
+                imagen: "https://i.ibb.co/mrpVhKMB/Remera-soda-estereo.png",
                 edicionLimitada: false,
                 tallesStock: { "S": 15, "M": 20, "L": 15, "XL": 10 }
             },
             {
-                nombre: "Remera Gotán Acid Wash",
-                descripcion: "Efecto gastado gris oscuro con vibras de los 2000 y el Obelisco de fondo.",
+                nombre: "Remera Gotán",
+                descripcion: "Corte urbano, Nirvana.",
                 precio: 26000,
-                imagen: "",
-                edicionLimitada: true,
+                imagen: "https://i.ibb.co/LDqFP0rW/Remera-Nirvana.png",
+                edicionLimitada: false,
                 tallesStock: { "S": 3, "M": 7, "L": 5, "XL": 2 }
             },
             {
-                nombre: "Remera Malevo Street",
-                descripcion: "Drop exclusivo con estampa trasera minimalista de la cultura de esquina.",
+                nombre: "Remera Malevo",
+                descripcion: "Drop exclusivo Aghanims.",
                 precio: 21500,
-                imagen: "",
+                imagen: "https://i.ibb.co/ZpzvgvmN/Remera-Aghanims.png",
                 edicionLimitada: false,
                 tallesStock: { "S": 8, "M": 14, "L": 12, "XL": 6 }
             },
             {
-                nombre: "Remera Bandoneón Heavy Metal",
-                descripcion: "Gráfica disruptiva que fusiona el tango clásico con la estética metalera urbana.",
+                nombre: "Remera Heavy",
+                descripcion: "Algodon peinado con estampa Saints.",
                 precio: 23000,
-                imagen: "",
-                edicionLimitada: false,
+                imagen: "https://i.ibb.co/yczSR41s/Remera-saint.png",
+                edicionLimitada: true,
                 tallesStock: { "S": 10, "M": 18, "L": 14, "XL": 8 }
             },
             {
-                nombre: "Remera Arraby Boxy Fit",
-                descripcion: "Silueta cuadrada, talle ancho y bordado sutil dorado en el pecho.",
+                nombre: "Remera ancha",
+                descripcion: "Con estampa Noble,.",
                 precio: 27500,
-                imagen: "",
+                imagen: "https://i.ibb.co/k6wL8hcL/Remera-Noble.png",
                 edicionLimitada: true,
                 tallesStock: { "S": 4, "M": 6, "L": 6, "XL": 3 }
             },
             {
-                nombre: "Remera Rebusque White",
-                descripcion: "Blanco puro, algodón pesado y estampa reflectiva ideal para la noche.",
+                nombre: "Remera Rebusque",
+                descripcion: "Rebusque puro, algodón con estampa The eyes.",
                 precio: 22000,
-                imagen: "",
+                imagen: "https://i.ibb.co/QvXGWxqD/Remera-the-eyes.png",
                 edicionLimitada: false,
                 tallesStock: { "S": 12, "M": 15, "L": 15, "XL": 9 }
             },
             {
-                nombre: "Remera Milonga Dark Drop",
+                nombre: "Remera Love Drop",
                 descripcion: "Edición medianoche con detalles en contratono y costuras reforzadas.",
                 precio: 25000,
-                imagen: "",
+                imagen: "https://i.ibb.co/ch1HcgB0/Remera-love.png",
                 edicionLimitada: true,
                 tallesStock: { "S": 2, "M": 5, "L": 8, "XL": 4 }
             },
             {
-                nombre: "Gorra Trucker Arrabalera",
-                descripcion: "Frente acolchado con parche clásico bordado de la marca y red negra.",
+                nombre: "Gorra Arrabalera",
+                descripcion: "Frente acolchado con parche clásico bordado de NY.",
                 precio: 14000,
-                imagen: "",
+                imagen: "https://i.ibb.co/NdgNn4mQ/Gorra-ny.png",
                 edicionLimitada: false,
                 tallesStock: { "S": 10, "M": 25, "L": 20, "XL": 0 }
             },
             {
-                nombre: "Cap Dad Hat 2die4",
-                descripcion: "Gorra de pana gastada con visera curva y hebilla de bronce regulable.",
+                nombre: "Cap Dad Hat",
+                descripcion: "Gorra de pana gastada con visera curva, regulable.",
                 precio: 16500,
-                imagen: "",
+                imagen: "https://i.ibb.co/9mMZvjp0/gorra-letras.png",
                 edicionLimitada: true,
                 tallesStock: { "S": 0, "M": 15, "L": 15, "XL": 0 }
-            },
-            {
-                nombre: "Gorra Snapback Esquina",
-                descripcion: "Visera plana, estructura rígida y bordado en alto relieve 3D frontal.",
-                precio: 15500,
-                imagen: "",
-                edicionLimitada: false,
-                tallesStock: { "S": 0, "M": 12, "L": 18, "XL": 0 }
             },
             {
                 nombre: "Gorra Beanie Conventillo",
                 descripcion: "Gorro de lana tejido premium doble capa para bancar el frío de la calle.",
                 precio: 12000,
-                imagen: "",
+                imagen: "https://i.ibb.co/MyXs9jDc/Gorra-North.png",
                 edicionLimitada: false,
                 tallesStock: { "S": 20, "M": 20, "L": 0, "XL": 0 }
             },
             {
-                nombre: "Gorra Trucker Fantasma Gold",
-                descripcion: "Detalles dorados sobre fondo negro satinado. Un clásico del pabellón.",
+                nombre: "Gorra Rose Gold",
+                descripcion: "Detalles blancos sobre fondo azul. Un clásico del pabellón.",
                 precio: 18000,
-                imagen: "",
+                imagen: "https://i.ibb.co/fVn21nkL/Gorra-rose.png",
                 edicionLimitada: true,
                 tallesStock: { "S": 0, "M": 8, "L": 8, "XL": 0 }
             },
             {
-                nombre: "Cap Camuflada Lunfarda",
-                descripcion: "Estampado militar urbano sutil con cierre strapback de cuero.",
+                nombre: "Cap Lunfarda",
+                descripcion: "Estampado urbano clasico LA.",
                 precio: 15000,
-                imagen: "",
+                imagen: "https://i.ibb.co/JRf5LfGf/Gorra-LA.png",
                 edicionLimitada: false,
                 tallesStock: { "S": 0, "M": 10, "L": 10, "XL": 0 }
             },
             {
-                nombre: "Gorra Piluso Malevo",
-                descripcion: "Bucket hat de gabardina negra pesada con etiqueta tejida lateral.",
+                nombre: "Gorra Malevo Clasica",
+                descripcion: "Bucket hat de gabardina negra Nike tejida.",
                 precio: 13500,
-                imagen: "",
+                imagen: "https://i.ibb.co/6cd2K4ds/Gorra-Nike.png",
                 edicionLimitada: false,
                 tallesStock: { "S": 5, "M": 15, "L": 10, "XL": 0 }
             },
             {
-                nombre: "Gorra Trucker 2die4 Limited",
-                descripcion: "La joya de la corona. Parche de cuero grabado a fuego y visera tracker.",
+                nombre: "Gorra Clasica bordada",
+                descripcion: "Parche de adidas y visera curva.",
                 precio: 19500,
-                imagen: "",
+                imagen: "https://i.ibb.co/m5PxxfpD/Gorra-adidas.png",
                 edicionLimitada: true,
                 tallesStock: { "S": 0, "M": 5, "L": 5, "XL": 0 }
             }
@@ -172,10 +164,10 @@ const conectarEjecutar = async () => {
         ];
 
         await Producto.insertMany(productosPrueba);
-        console.log('¡16 prendas cargadas exitosamente!');
+        console.log('¡Ropa cargada!');
 
         await Contacto.insertMany(contactosPrueba);
-        console.log('¡4 mensajes de contacto cargados exitosamente!');
+        console.log('¡Mensajes cargados!');
 
         mongoose.disconnect();
         console.log('Desconectado de la base de datos.');
